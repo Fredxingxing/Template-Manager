9247f980391e3e106bda3f9daa860aab20ed8f47
Fredxingxing <33107804+Fredxingxing@users.noreply.github.com>
2018-06-22 01:05:13 +0800

    增加i18n
    detail里的base、审核的base、map中英转换还未完

---
+++ src/data/en-US.js
@@ -0,0 +1,16 @@
+export const lang={
+  introduction:'The Template Manager is designed to help players better understand each hero\'s position, function, role, and relationship with other heroes.m' +
+    'For eaxmple,The synergy, such as Vera Aurel, The restrained by the relations between the werewolf and Arthas, and the restraint, such as Tyrrell, restrained Maiev.' +
+' Back pot assistant team develops together, if you are interested, please add QQ group: 417011860',
+ title:'Hero',
+  base:'Base',
+  herorelation:'Hero Relation',
+  synergy:'Synergy',
+  restraint:'Restraint',
+  BeRestrained:'BeRestrained',
+  masterView:'Audit queries and processing',
+  submit:'Submit',
+  lastPage:'LastPage',
+  nextPage:'NextPage',
+  exitMasterView:'Exit',
+}
